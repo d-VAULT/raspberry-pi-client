f8fc06bec167587c3fb0b17ead3b7b1d1e7356cf
Timen Olthof <timen@timenolthof.nl>
2018-04-06 20:03:35 +0200

    feat: naive integration of frontend design

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import axios from 'axios';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
@@ -25,13 +25,73 @@ class App extends Component {
     const { identity } = this.state;
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">D-Vault</h1>
-        </header>
-        <p className="App-intro">
-          { identity }
-        </p>
+        <div id="wrap">
+
+        	<div id="supplier">
+        		<div id="title">
+        		Supplier
+        		</div>
+          	<img src="/images/nuon_off.png" id="image1"/>
+          	<img src="/images/budget_off.png" id="image1"/>
+          	<img src="/images/bron_off.png" id="image1"/>
+
+        		<img src="/images/share.png" id="image2"/>
+        	</div>
+
+
+
+        	<div id="power">
+        		<div id="title">
+        		  My power ({ identity })
+        		</div>
+          	<img src="/images/bulb_off.png" id="image2"/>
+          	<img src="/images/car_off.png" id="image2"/>
+          	<img src="/images/mill_off.png" id="image2"/>
+        	</div>
+
+
+
+        	<div id="usage">
+        		<div id="title">
+        		</div>
+
+        		<div id="usage_sub">
+        			<div id="title_sub">
+        			My usage
+        			</div>
+        		1000
+        		</div>
+
+        		<div id="usage_sub">
+        			<div id="title_sub">
+        			Generating
+        			</div>
+        		300
+        		</div>
+
+        		<div id="usage_sub">
+        			<div id="title_sub">
+        			Demand
+        			</div>
+        		700
+        		</div>
+
+        		<div id="usage_sub">
+        			<div id="title_sub">
+        			Supply
+        			</div>
+        		0
+        		</div>
+
+        	<div id="logo">
+        		<img src="/images/logo.png" id="image3" />
+
+        	</div>
+
+
+        	</div>
+
+        </div>
       </div>
     );
   }
