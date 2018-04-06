c95b41169878dfac6016872c80eea941ed401014
Erwin Rooijakkers <erwin.rooijakkers@alliander.com>
2018-04-06 21:39:15 +0200

    fix: correct variable name

--- backend/energy_supplier/group_member.py
+++ backend/energy_supplier/group_member.py
@@ -3,6 +3,6 @@ class GroupMember:
     IOTA addresses and public key of the group member
     """
 
-    def __init__(self, address, publicKey):
+    def __init__(self, address, public_key):
         self.address = address
         self.public_key = public_key
