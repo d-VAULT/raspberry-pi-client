3eba8571830cafec892b7691a8817d24baf34ce9
Erwin Rooijakkers <erwin.rooijakkers@alliander.com>
2018-04-07 20:57:53 +0200

    chore: use hard coded aggregator address

--- backend/config.py
+++ backend/config.py
@@ -1,6 +1,6 @@
 import os
 
 seed = os.environ['SEED']
-aggregator_address = os.environ['AGGREGATOR_ADDRESS']
+aggregator_address = 'XBC9JOMDXVRTS9VRLULWYBHNGK9BMYMQKKZFDNFFHTJCNCT9LQJQQIBF9PEAUZVRPCRDMXFONZLSYBVJA'
 
 provider = 'http://node01.testnet.iotatoken.nl:16265'
