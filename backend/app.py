bbda923da32e869d520ba2541520b72270cd0d03
Timen Olthof <timen@timenolthof.nl>
2018-04-07 10:29:32 +0200

    feat: serve frontend through backend

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,21 @@
-from flask import Flask
-app = Flask(__name__)
+import os
+from flask import Flask, send_from_directory
 
+app = Flask(__name__, static_folder='../frontend/build')
+
+# Serve React App
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>')
+def serve(path):
+    if(path == ""):
+        return send_from_directory('../frontend/build', 'index.html')
+    else:
+        if(os.path.exists("../frontend/build/" + path)):
+            return send_from_directory('../frontend/build', path)
+        else:
+            return send_from_directory('../frontend/build', 'index.html')
+
+# Serve API backend
 @app.route('/api/identity')
 def identity():
     return 'I am Pi number 5'
