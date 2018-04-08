d8b85ea48be6651e9154007d538c817c83144e61
Ruud Hendrikx <rjghendrikx@gmail.com>
2018-04-08 05:07:37 +0200

    feat: paillier encrypt and decrypt functions with working example

--- backend/cryptography/paillier.py
+++ backend/cryptography/paillier.py
@@ -6,8 +6,21 @@ from phe import paillier
 class Paillier(object):
     """Python public key encryption and partial homomorphic encryption via paillier"""
     def __init__(self, public_key, private_key):
+        if not public_key and not private_key:
+            public_key, private_key = paillier.generate_paillier_keypair()
+        self.paillier = paillier
         self.public_key = public_key
         self._private_key = private_key
+
+    def encrypt(self, value, public_key=None):
+        if public_key==None:
+            public_key=self.public_key
+        return public_key.encrypt(value)
+
+    def decrypt(self, value, private_key=None):
+        if private_key==None:
+            private_key=self._private_key
+        return private_key.decrypt(value)
 
     @classmethod
     def from_keypair_dict(cls, keypair_dict):
@@ -49,3 +62,9 @@ class Paillier(object):
     @staticmethod
     def _public_key_from_dict(public_key_dict):
         return paillier.PaillierPublicKey(public_key_dict['n'])
+
+p = Paillier(None, None)
+e = p.encrypt(52)
+print("ciphertext: \n\n", e.ciphertext())
+d = p.decrypt(e)
+print("\ndecoded: ", d)
