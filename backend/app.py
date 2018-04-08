fc5d0af4917da1ffc79300a6540da5b286f4b833
Ruud Hendrikx <rjghendrikx@gmail.com>
2018-04-08 02:05:41 +0200

    feat: parameterized cycle triggering and reporting a static actual meter value

--- backend/app.py
+++ backend/app.py
@@ -1,16 +1,15 @@
-import os
-import netifaces
-from flask import Flask, send_from_directory, request, jsonify
-from smart_meter.smart_meter import SmartMeter, SmartMeterProfile
-import time
 import atexit
+import netifaces
+import os
+import random
+import time
+from datetime import datetime, timedelta
 
 from apscheduler.schedulers.background import BackgroundScheduler
 from apscheduler.triggers.interval import IntervalTrigger
-from apscheduler.triggers.date import DateTrigger
-from datetime import datetime, timedelta
-
+from flask import Flask, send_from_directory, request, jsonify
 from push_sum import PushSum
+from smart_meter.smart_meter import SmartMeter, SmartMeterProfile
 
 # Get LAN ip
 interfaces = netifaces.interfaces()
@@ -19,73 +18,93 @@ if 'wlan0' in interfaces:
 else:
     ip = "X"
 
-# set push sum parameters
-value = 10
-total_rounds = 30
 
-def do_push_sum_cycle(value, total_rounds, start_date_sum):
-    ps = PushSum(value, total_rounds=total_rounds)
+# define the triggering of rounds in one push sum cycle
+def do_push_sum_cycle(value, total_rounds, cycle_time):
+    start_date_sum = datetime.now()
+    end_date_sum = start_date_sum + timedelta(seconds=cycle_time - 2)
+
+    # report start of new cycle to console
+    print("NEW CYCLE STARTED: ", start_date_sum.strftime("%A, %d. %B %Y %I:%M:%S %p"))
+    print("CYCLE WILL END AT: ", end_date_sum.strftime("%A, %d. %B %Y %I:%M:%S %p"))
+
+    # initialise push sum object
+    ps = PushSum(value,
+                 total_rounds=total_rounds,
+                 cycle_time_seconds=cycle_time)
     round_time_seconds = ps.round_time_seconds
 
+    # define triggering function and report to console
     def do_push_sum():
-        print(time.strftime("%A, %d. %B %Y %I:%M:%S %p"))
+        print("PUSH SUM ITERATION: ", time.strftime("%A, %d. %B %Y %I:%M:%S %p"))
         ps.iterate_round()
 
     scheduler = BackgroundScheduler()
     scheduler.start()
     scheduler.add_job(
         func=do_push_sum,
-        start_date=start_date_sum,
-        trigger=IntervalTrigger(seconds=round_time_seconds),
+        trigger=IntervalTrigger(start_date=start_date_sum,
+                                seconds=round_time_seconds,
+                                end_date=end_date_sum),
         id='do_push_sum_cycle',
         name='Perform push sum work',
         replace_existing=False)
 
-    # Shut down the scheduler when exiting the app
+    # shut down the scheduler when exiting the app
     atexit.register(lambda: scheduler.shutdown())
 
 
-def start_new_cycle(value, total_rounds, start_date_cycle):
+# define the triggering of cycles
+def start_clocked_cycles(start_date_cycle, value, total_rounds, cycle_time):
+    print("CYCLE SCHEDULER STARTED: ", time.strftime("%A, %d. %B %Y %I:%M:%S %p"))
 
-    start_date_sum = start_date_cycle + timedelta(seconds=2)
+    # initialize scheduler and add job
     scheduler = BackgroundScheduler()
     scheduler.start()
-
     scheduler.add_job(
         func=do_push_sum_cycle,
-        args=[value, total_rounds, start_date_sum],
-        trigger=DateTrigger(start_date_cycle),
+        args=[value, total_rounds, cycle_time],
+        trigger=IntervalTrigger(start_date=start_date_cycle,
+                                seconds=cycle_time),
         id='start_new_cycle',
         name='Starts a new push sum',
-        replace_existing=False)
+        replace_existing=True)
 
     # Shut down the scheduler when exiting the app
     atexit.register(lambda: scheduler.shutdown())
 
 
-#push_sum_object = PushSum(value, total_rounds=total_rounds)
-#ps = push_sum_object
-#do_push_sum_cycle(value, total_rounds, ps)
-start_date_cycle = datetime(2018, 4, 8, 00, 25, 1)
-start_new_cycle(value, total_rounds, start_date_cycle)
+# set random profile of meter and get value
+random_profile = [bool(random.getrandbits(i)) for i in [1, 1, 1]]
+meter = SmartMeter(SmartMeterProfile(*random_profile))
+value = meter.get_data().demand - meter.get_data().supply
 
+# set push sum parameters
+total_rounds = 30
+cycle_time = 300
 
+# real start_time = start_date_cycle + N * cycle_time
+start_date_cycle = datetime(2018, 4, 8, 00, 00, 1)
 
-meter = SmartMeter(SmartMeterProfile(True, False, False))
+# start the clocked cycles scheduler
+start_clocked_cycles(start_date_cycle, value, total_rounds, cycle_time)
 
+# initialise the Flask app
 app = Flask(__name__, static_folder='../frontend/build')
+
 
 # Serve React App
 @app.route('/', defaults={'path': ''})
 @app.route('/<path:path>')
 def serve(path):
-    if(path == ""):
+    if (path == ""):
         return send_from_directory('../frontend/build', 'index.html')
     else:
-        if(os.path.exists("../frontend/build/" + path)):
+        if (os.path.exists("../frontend/build/" + path)):
             return send_from_directory('../frontend/build', path)
         else:
             return send_from_directory('../frontend/build', 'index.html')
+
 
 # Serve API backend
 @app.route('/api/identity')
@@ -100,6 +119,7 @@ def identity():
         }
     }), 200
 
+
 @app.route('/api/update-profile', methods=['POST'])
 def update_profile():
     if not request.json or not 'car' in request.json:
@@ -113,6 +133,7 @@ def update_profile():
             'wind': meter.profile.wind
         }
     }), 200
+
 
 @app.route('/api/data')
 def data():
